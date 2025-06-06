fba25ed922a50e0053f7f3404b95b3374f4f4884
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-06 12:09:11 -0300

    update firebase

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDn5SsldbMPI-fX0Cy2sdEDb6hkUnGm2Hc",
   authDomain: "hora-da-protecao.firebaseapp.com",
   projectId: "hora-da-protecao",
-  storageBucket: "hora-da-protecao.firebasestorage.app",
+  storageBucket: "hora-da-protecao.appspot.com",
   messagingSenderId: "479715828390",
   appId: "1:479715828390:web:68d1eb1454962cc044ca67",
   measurementId: "G-Q4207EHJMP"
