5f3e8306120939845d9b7ccfd7e60ffa4c598305
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-07 16:09:52 -0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,17 +3,20 @@ import { useColorScheme } from 'react-native';
 import { ThemeProvider } from '@react-navigation/native';
 import { DarkTheme, DefaultTheme } from '@react-navigation/native';
 import React from 'react';
+import { AuthProvider } from '../context/AuthProvider'; // Ajuste o caminho conforme necessário
 
 export default function Layout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        />
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
