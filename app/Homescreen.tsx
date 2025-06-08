a79e0bcb6700dfa26383decab591ca9af37941ff
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-08 03:39:17 -0300

    update homescreen

--- app/Homescreen.tsx
+++ app/Homescreen.tsx
@@ -11,7 +11,7 @@ const HomeScreen = () => {
       <Text style={styles.title}>home</Text>
 
       <Image
-        source={require('@/assets/images/user-profile-icon-free-vector.jpg')}
+        source={require('../assets/iamges/usericon.png')}
         style={styles.userIcon}
       />
       <Text style={styles.username}>username</Text>
