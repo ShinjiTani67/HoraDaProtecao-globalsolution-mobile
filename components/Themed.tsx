f44db6de6f367f0ca47f2121fa77460c8d08c3b0
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-08 03:56:55 -0300

    update themed

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -1,17 +1,59 @@
-// components/Themed.tsx
-import { Text as DefaultText, View as DefaultView } from 'react-native';
+import * as React from 'react';
+import {
+  Text as DefaultText,
+  View as DefaultView,
+  useColorScheme,
+  TextProps as DefaultTextProps,
+  ViewProps as DefaultViewProps,
+} from 'react-native';
 
-export function Text(props: DefaultText['props']) {
-  return <DefaultText {...props} />;
-}
-
-export function View(props: DefaultView['props']) {
-  return <DefaultView {...props} />;
-}
-
-
-export default {
-  primary: '#007AFF',
-  background: '#ffffff',
-  text: '#000000',
+type ThemeProps = {
+  lightColor?: string;
+  darkColor?: string;
 };
+
+function useThemeColor(
+  props: { light?: string; dark?: string },
+  colorName: 'text' | 'background'
+) {
+  const theme = useColorScheme();
+  const colorFromProps = props[theme ?? 'light'];
+
+  if (colorFromProps) {
+    return colorFromProps;
+  }
+
+  // Fallback colors (personalize se quiser)
+  const fallbackColors = {
+    light: {
+      text: '#000',
+      background: '#fff',
+    },
+    dark: {
+      text: '#fff',
+      background: '#000',
+    },
+  };
+
+  return fallbackColors[theme ?? 'light'][colorName];
+}
+
+export type TextProps = ThemeProps & DefaultTextProps;
+export type ViewProps = ThemeProps & DefaultViewProps;
+
+export function Text(props: TextProps) {
+  const { style, lightColor, darkColor, ...otherProps } = props;
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+
+  return <DefaultText style={[{ color }, style]} {...otherProps} />;
+}
+
+export function View(props: ViewProps) {
+  const { style, lightColor, darkColor, ...otherProps } = props;
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    'background'
+  );
+
+  return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+}
